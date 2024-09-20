9264f372e4ccfd8edfabdcf7cc4c6b56e5c4b0a7
Quby <135136021+Qubbby@users.noreply.github.com>
2024-09-20 09:14:09 +0800

    Update __init__.py
    Standardize the format of the module path

--- example/FluentUI/__init__.py
+++ example/FluentUI/__init__.py
@@ -4,7 +4,7 @@ import os
 from PySide6.QtQml import QQmlApplicationEngine
 
 def init(engine:QQmlApplicationEngine):
-    current_module_path = os.path.dirname(os.path.abspath(__file__))+"/qml"
+    current_module_path = os.path.dirname(os.path.abspath(__file__)).replace("\\", "/")+"/qml"
     print("FluentUI plugin dir ->"+current_module_path+"/FluentUI")
     engine.addImportPath(current_module_path)
     
